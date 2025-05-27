89677edf5dbe1b52f89f5c1d51d12a61b2305e46
hamadKhan21 <hamad2k9@gmail.com>
2025-05-26 22:58:39 -0500

    Add files via upload

---
+++ Migrations/20240925073327_RemoveFieldName.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFieldName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Coupones");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Coupones",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Coupones",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "IsActive",
+                value: false);
+
+            migrationBuilder.UpdateData(
+                table: "Coupones",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "IsActive",
+                value: false);
+        }
+    }
+}
